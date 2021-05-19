a4b154829a020790ac5fcab4204f727720e3aa86
unknown <kai.wolff.92@gmail.com>
2021-05-19 17:14:47 +0100

    extra tasks completed

--- calculate_birthyear.py
+++ calculate_birthyear.py
@@ -1,12 +1,17 @@
+import time
+from datetime import date
+now = date.today()
 name = input("\nHello, what is your name? ")
-age = int(input("\nThank you, what is your age? "))
-current_year = 2021
+birthday = input("\nThank you, please enter your birthday in format dd/mm: ")
+age = int(input("\nThank you, and how old are you? "))
 
-birthday_happened = input("Has your birthday already happened this year? (y/n) ")
+birthday_timed = time.strptime(birthday, "%d/%m") #this should turn the input string from birthday into a struct_time
 
-if birthday_happened.upper() == "Y":
-    print(f"OMG, you are {age} years old so you born in {current_year - age}")
-elif birthday_happened.upper() == "N":
-    print(f"OMG, you are {age} years old so you born in {current_year - 1 - age}")
+if birthday_timed[1] > now.month:
+    print(f"OMG, you are {age} years old so you were born in {now.year - 1 - age}")
+
+elif birthday_timed[1] == now.month and birthday_timed[2]:
+    print(f"OMG, you are {age} years old so you were born in {now.year - 1 - age}")
+
 else:
-    print("Unfortunately, I didn't recognise your answer. Please use y for yes, or n for no")
+    print(f"OMG, you are {age} years old so you were born in {now.year  - age}")
